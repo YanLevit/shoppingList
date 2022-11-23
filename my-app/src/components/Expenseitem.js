329a035122e8a26e277c188afd1db87859f310e3
Yan Levit <yan2045@gmail.com>
2022-11-23 17:52:00 +0200

    some further styling

--- my-app/src/components/Expenseitem.js
+++ my-app/src/components/Expenseitem.js
@@ -3,8 +3,12 @@ import './Expenseitem.css';
 function ExpenseItem(props) {
   return (
     <div className="expense-item" onClick={props.onClick}>
-      <div className="expense-item_description"> Title : {props.title}</div>
-      <div className="expense-item_price"> Amount : {props.amount} </div>
+      <div className="expense-item_description">
+        <b>Title :</b> {props.title}
+      </div>
+      <div className="expense-item_price">
+        <b>Amount :</b> {props.amount}{' '}
+      </div>
       <img className="expense-item_image" src={props.url} alt="" />
     </div>
   );
